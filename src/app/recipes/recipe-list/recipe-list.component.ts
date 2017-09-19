17c97063e9a1128af1172d84ae3baa743a16c0ca
Gabriele Luz <gabrieleluz@users.noreply.github.com>
2017-09-18 21:27:33 -0300

    Lecture 49

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe('Name', 'Lorem ipsum comidum dolor na barrigum', 'https://goo.gl/shS4r1'),
+		new Recipe('Name', 'Lorem ipsum comidum dolor na barrigum', 'https://goo.gl/shS4r1')
+	];
 
   constructor() { }
 
